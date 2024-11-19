0616cfb6e176fd53b3cb05a1eea49bee8092df1f
Adam Nelson-Archer <47440321+adamnelsonarcher@users.noreply.github.com>
2024-11-19 15:40:20 -0600

    updated firebase to use local vars

--- src/config/firebase-admin.js
+++ src/config/firebase-admin.js
@@ -1,5 +1,11 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json'); // You'll need to download this from Firebase Console
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
